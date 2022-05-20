fd84c157d088d7a264a16fb7ef5906ff0cd843cf
Jason Guo <sg763@cornell.edu>
2022-05-20 18:47:36 -0400

    Initial commit

---
+++ rust/scripts/src/main.rs
@@ -0,0 +1,25 @@
+// fib_iter(n) is the nth fibonacci number
+fn fib_iter(n: u32) -> u64 {
+    let mut counter = 1;
+    let mut last = 0;
+    let mut new = 1;
+    while counter < n {
+        let temp = new;
+        new = last + new;
+        last = temp;
+        counter += 1;
+    }
+    new
+}
+
+fn print_fib() {
+    let mut counter = 10;
+    while counter > 0 {
+        println!("{}", fib_iter(10 - counter + 1));
+        counter -= 1;
+    }
+}
+
+fn main() {
+    
+}
---
+++ rust/scripts/target/debug/deps/scripts-a985496f56a481df.d
@@ -0,0 +1,5 @@
+/home/jason/workspace/misc/rust/scripts/target/debug/deps/scripts-a985496f56a481df.rmeta: src/main.rs
+
+/home/jason/workspace/misc/rust/scripts/target/debug/deps/scripts-a985496f56a481df.d: src/main.rs
+
+src/main.rs:
