21673cd2874427407a39a168f348e8cbfc174998
Jason Guo <jasonguo0815@gmail.com>
2022-05-22 00:33:25 -0400

    euler 420 overcounting

--- rust/scripts/src/main.rs
+++ rust/scripts/src/main.rs
@@ -20,6 +20,63 @@ fn print_fib() {
     }
 }
 
+use std::collections::HashMap;
+const N : u64 = 50;
+
+fn matrix_was_seen_twice(hm: &mut HashMap<(u64, u64, u64,u64), bool>, a1: u64, b1: u64, c1: u64, d1: u64) -> bool{
+    let a = a1*a1 + b1*c1;
+    let b = a1*b1 + b1*d1;
+    let c = a1*c1 + c1*d1;
+    let d = b1*c1 + d1*d1;
+    if a + d >= N{
+        false
+    }else{
+        let mat = (a,b,c,d);
+        match hm.get_key_value(&(a,b,c,d)){
+            Some((_, true)) => false,
+            Some((_, false)) => {hm.insert(mat, true); true},
+            None => {hm.insert(mat, false); false}
+        }
+    }
+}
+
+fn e420(n: u64) -> u64{
+    let mut counter = 0;
+    let mut hm : HashMap<(u64, u64, u64,u64), bool> = HashMap::new();
+
+    let mut a1 = 0;
+    let mut b1 = 0;
+    let mut c1 = 0;
+    let mut d1 = 0;
+    let sqrtn = f64::sqrt(n as f64) as u64;
+    let halfn = n/2;
+    while a1 <= sqrtn{
+        b1 = 0;
+        c1 = 0;
+        d1 = 0;
+        while d1 <= sqrtn{
+            if matrix_was_seen_twice(&mut hm, a1, b1, c1, d1){
+                counter += 1;
+            }
+            b1 = 1;
+            c1 = 1;
+            while b1 <= halfn{
+                c1 = 1;
+                while c1 <= halfn/b1 + 1{
+                    if matrix_was_seen_twice(&mut hm, a1, b1, c1, d1){
+                        counter += 1;
+                    }
+                    c1 += 1;
+                }
+                b1 += 1;
+            }
+            d1 += 1;
+        }
+        a1 += 1;
+    }
+    counter
+}
+
 fn main() {
-    
+    println!("{}", e420(50));
 }
